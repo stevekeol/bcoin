c8585af2d9101f905de2b24893d78717f64699e5
stevekeol <stevekeol.x@gmail.com>
2021-02-11 17:27:28 +0800

    add: comment in workers/child-browser.js

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-const bio = require('bufio');
-const assert = require('bsert');
+// const bio = require('bufio');
+// const assert = require('bsert');
 
-const bw = bio.write(); // 构建 BufferWriter对象
-console.log(bw); 
-bw.writeU64(100);
-console.log(bw); 
-bw.writeString('foo')
-console.log(bw); 
+// const bw = bio.write(); // 构建 BufferWriter对象
+// console.log(bw); 
+// bw.writeU64(100);
+// console.log(bw); 
+// bw.writeString('foo')
+// console.log(bw); 
 
-const data = bw.render(); //构建 Buffer对象
+// const data = bw.render(); //构建 Buffer对象
 
-const br = bio.read(data); //构建 BufferReader对象
-assert(br.readU64() === 100);
-assert(br.readString(3) === 'foo')
+// const br = bio.read(data); //构建 BufferReader对象
+// assert(br.readU64() === 100);
+// assert(br.readString(3) === 'foo')
+
+// //--------------------------
+
+let data = Buffer.from([1, 2, 3, 4]);
+data.__proto__ = Buffer.prototype;
+
+console.log(data);
